2f60ceac5d5fbc0bf01a13fce3c200c1d41ad1f9
drskovatereza <drskova.tereza@gmail.com>
2016-09-07 11:17:09 +1200

    first angular test working

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 
-var app = angular.module("playersApp", ["playersCtrl"]);
+//var app = angular.module("playersApp", ["playersCtrl"]);
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
--- js/controllers/playersCtrl.js
+++ js/controllers/playersCtrl.js
@@ -1,7 +1,11 @@
 
-
+/*
 angular.module("playersCtrl", [])
 
-
+.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
+*/
 
 

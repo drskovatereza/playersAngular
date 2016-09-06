248295cac54292eafaac755b822be094137f013e
drskovatereza <drskova.tereza@gmail.com>
2016-09-07 11:30:18 +1200

    displaying data from array within JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 
-//var app = angular.module("playersApp", ["playersCtrl"]);
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+var app = angular.module('playersApp', []);
+app.controller('playersCtrl', function($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+
+    $scope.playersData = 
+    [
+    {
+    	name : "player1"
+    },
+    {
+    	name : "player2"
+    }
+    ]
+
+   
 });
--- js/controllers/playersCtrl.js
+++ js/controllers/playersCtrl.js
@@ -1,11 +1,6 @@
 
-/*
-angular.module("playersCtrl", [])
 
-.controller('myCtrl', function($scope) {
+angular.module('playersApp').controller('playersCtrl',function($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
 });
-*/
-
-
